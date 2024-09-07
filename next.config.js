47e4f5b5016e12368a65f3a88824c5cfcb020b14
Kent Langley <jkl@kentlangley.com>
2024-09-06 17:35:40 -0700

    version 10

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['@mantine/core', '@mantine/hooks'],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+}
 
 module.exports = nextConfig
